60cab912d218aa15e0fe4dafcb620660a104535d
yangsong <449711478@qq.com>
2019-03-21 20:21:37 +0800

    update camera

--- python/OpenCV/c4/c41.py
+++ python/OpenCV/c4/c41.py
@@ -70,7 +70,7 @@ def showCameraCaptureAndFace():
 
     while success and cv2.waitKey(1) == -1 and not clicked:
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray,1.3,5)
+        faces = face_cascade.detectMultiScale(gray,1.3,2)
         print(faces)
         for(x,y,w,h) in faces:
             frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
@@ -81,8 +81,44 @@ def showCameraCaptureAndFace():
     pass
 
 
+def showCameraCaptureAndFilterFace():
+    clicked  = False
+    def omMouse(event,x,y,flags,param):
+        if event == cv2.EVENT_LBUTTONUP:
+            clicked = True
+        pass
+    cameraCapture = cv2.VideoCapture(0)
+    cv2.namedWindow('CameraWindow')
+    cv2.setMouseCallback('CameraWindow',omMouse);
+    success,frame = cameraCapture.read()
+    face_cascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_default.xml')
+
+    while success and cv2.waitKey(1) == -1 and not clicked:
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray,1.3,5)
+        for(x,y,w,h) in faces:
+            face_frame = gray[y:y+h,x:x+w]
+            face_frame0 = frame[y:y + h, x:x + w]
+            binary = cv2.Canny(face_frame, 50, 150, apertureSize=3, L2gradient=False);
+            contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.drawContours(face_frame0, contours, -1, (255, 0, 0), 1)
+            #for i in contours:
+                #roiArea = cv2.boundingRect(i)
+                #cropImg = face_frame0[roiArea[1]:roiArea[1] + roiArea[3], roiArea[0]: roiArea[0] + roiArea[2]]
+                #cropImg = cv2.bilateralFilter(cropImg, 0,150,15)
+                #cropImg = cv2.GaussianBlur(cropImg, (15, 15), 0)
+                #face_frame0[roiArea[1]:roiArea[1] + roiArea[3], roiArea[0]: roiArea[0] + roiArea[2]] = cropImg
+            frame[y:y + h, x:x + w] = face_frame0
+            #frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),1)
+
+        cv2.imshow('CameraWindow',frame)
+        success,frame = cameraCapture.read()
+    cv2.destroyWindow("CameraWindow")
+    cameraCapture.release()
+    pass
+
 def main():
-    showCameraCaptureAndFace()
+    showCameraCaptureAndFilterFace()
     pass
 
 main()
