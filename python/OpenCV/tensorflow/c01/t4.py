beae955c7db879ccf8dbf762d4207c112f2e7007
yangsong <449711478@qq.com>
2019-04-08 23:38:25 +0800

    Merge branch 'master' of https://github.com/tomsnail/OpenCVDemo
    # Conflicts:
    #	python/OpenCV/.idea/OpenCV.iml
    #	python/OpenCV/.idea/misc.xml
    #	python/OpenCV/.idea/workspace.xml

--- python/OpenCV/tensorflow/c01/t4.py
+++ python/OpenCV/tensorflow/c01/t4.py
@@ -134,35 +134,33 @@ def mnist_op3():
     y = tf.placeholder(tf.float32,[None,10])
     #定义神经元dropout参数
     keep_prob = tf.placeholder(tf.float32)
+    lr = tf.Variable(0.001,dtype=tf.float32)
 
     #创建神经网络
 
-    W1 = tf.Variable(tf.truncated_normal([784,2000],stddev=0.1))
-    b1 = tf.Variable(tf.zeros([2000])+0.1)
+    W1 = tf.Variable(tf.truncated_normal([784,500],stddev=0.1))
+    b1 = tf.Variable(tf.zeros([500])+0.1)
     L1 = tf.nn.tanh(tf.matmul(x,W1)+b1)
     L1_drop = tf.nn.dropout(L1,keep_prob)
 
-    W2 = tf.Variable(tf.truncated_normal([2000, 2000], stddev=0.1))
-    b2 = tf.Variable(tf.zeros([2000]) + 0.1)
+    W2 = tf.Variable(tf.truncated_normal([500, 300], stddev=0.1))
+    b2 = tf.Variable(tf.zeros([300]) + 0.1)
     L2 = tf.nn.tanh(tf.matmul(L1_drop, W2) + b2)
     L2_drop = tf.nn.dropout(L2, keep_prob)
 
-    W3 = tf.Variable(tf.truncated_normal([2000, 1000], stddev=0.1))
-    b3 = tf.Variable(tf.zeros([1000]) + 0.1)
-    L3 = tf.nn.tanh(tf.matmul(L2_drop, W3) + b3)
-    L3_drop = tf.nn.dropout(L3, keep_prob)
 
-    W4 = tf.Variable(tf.truncated_normal([1000, 10], stddev=0.1))
+
+    W4 = tf.Variable(tf.truncated_normal([300, 10], stddev=0.1))
     b4 = tf.Variable(tf.zeros([10]) + 0.1)
-    prediction = tf.nn.softmax(tf.matmul(L3_drop,W4)+b4)
+    prediction = tf.nn.softmax(tf.matmul(L2_drop,W4)+b4)
 
-    # 二次代价函数
+    # 交叉熵
     # loss = tf.reduce_mean(tf.square(y - prediction))
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y,logits=prediction))
     # 梯度下降训练函数
 
     # train_step = tf.train.GradientDescentOptimizer(0.2).minimize(loss)
-    train_step = tf.train.AdamOptimizer(1e-2).minimize(loss)
+    train_step = tf.train.AdamOptimizer(lr).minimize(loss)
 
 
     # 初始化变量
@@ -176,16 +174,15 @@ def mnist_op3():
     # 开始训练
     with tf.Session() as sess:
         sess.run(init)
-        for epoch in range(31):
+        for epoch in range(51):
             print("start:",epoch)
+            sess.run(tf.assign(lr,0.001 * ( 0.95 ** epoch)))
             for batch in range(n_batch):
-                print("start batch :", batch)
                 batch_xs,batch_ys = mnist.train.next_batch(batch_size)
-                sess.run(train_step,feed_dict={x:batch_xs,y:batch_ys,keep_prob:0.3})
+                sess.run(train_step,feed_dict={x:batch_xs,y:batch_ys,keep_prob:1.0})
 
             test_acc = sess.run(accuracy,feed_dict={x:mnist.test.images,y:mnist.test.labels,keep_prob:1.0})
-            train_acc = sess.run(accuracy, feed_dict={x: mnist.train.images, y: mnist.train.labels, keep_prob: 1.0})
-            print(epoch,test_acc,train_acc)
+            print(epoch,test_acc)
 
     pass
 
